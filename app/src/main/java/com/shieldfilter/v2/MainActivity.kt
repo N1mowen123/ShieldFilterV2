f7cc11407cd2b1e00f5d492f0349961670eff08c
N1mowen123 <2804937533@qq.com>
2026-09-23 15:53:25 +0800

    Refactor MainActivity to simplify password setup

--- app/src/main/java/com/shieldfilter/v2/MainActivity.kt
+++ app/src/main/java/com/shieldfilter/v2/MainActivity.kt
@@ -1,64 +1,42 @@
 package com.shieldfilter.v2
 
 import android.content.Intent
-import android.media.projection.MediaProjectionManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.shieldfilter.v2.model.*
-import com.shieldfilter.v2.service.ScreenCaptureService
+import com.shieldfilter.v2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var password: String? = null
-    private val REQ_SCREEN_CAPTURE = 1002
-
-    private val styles = mapOf(
-        FemaleBodyPart.EYES to PartStyle(FemaleBodyPart.EYES, true, BlockMode.BLACK_WITH_TEXT),
-        FemaleBodyPart.CHEST to PartStyle(FemaleBodyPart.CHEST, true, BlockMode.MOSAIC),
-        FemaleBodyPart.ARMPITS to PartStyle(FemaleBodyPart.ARMPITS, true, BlockMode.BLACK_SOLID),
-        FemaleBodyPart.PELVIS to PartStyle(FemaleBodyPart.PELVIS, true, BlockMode.BLACK_WITH_TEXT),
-        FemaleBodyPart.FEET to PartStyle(FemaleBodyPart.FEET, true, BlockMode.MOSAIC)
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (!Settings.canDrawOverlays(this)) {
-            val i = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
-            startActivity(i)
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")))
         }
 
-        findViewById<android.widget.Button>(R.id.btnSetPwd).setOnClickListener {
+        findViewById<Button>(R.id.btnSetPwd).setOnClickListener {
             val et = EditText(this)
             AlertDialog.Builder(this)
-                .setTitle("设置解锁密码")
+                .setTitle("设置密码")
                 .setView(et)
-                .setPositiveButton("确定") {_,_ -> password = et.text.toString() }
+                .setPositiveButton("确定") { _, _ -> password = et.text.toString() }
                 .show()
         }
 
-        findViewById<android.widget.Button>(R.id.btnStart).setOnClickListener {
-            if(password.isNullOrEmpty()){
-                AlertDialog.Builder(this).setMessage("请先设置解锁密码").show()
-                return@setOnClickListener
+        findViewById<Button>(R.id.btnStart).setOnClickListener {
+            if (password.isNullOrEmpty()) {
+                AlertDialog.Builder(this).setMessage("请先设置密码").show()
+            } else {
+                AlertDialog.Builder(this).setMessage("防护已启动（演示版）").show()
             }
-            val mpm = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-            startActivityForResult(mpm.createScreenCaptureIntent(), REQ_SCREEN_CAPTURE)
-        }
-    }
-
-    override fun onActivityResult(rq: Int, res: Int, data: Intent?) {
-        super.onActivityResult(rq,res,data)
-        if(rq == REQ_SCREEN_CAPTURE && res == RESULT_OK && data != null){
-            val svc = Intent(this, ScreenCaptureService::class.java).apply {
-                putExtra("RESULT_CODE", res)
-                putExtra("PROJECTION_DATA", data)
-            }
-            startForegroundService(svc)
         }
     }
 }
